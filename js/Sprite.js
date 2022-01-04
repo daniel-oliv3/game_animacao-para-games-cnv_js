f2f174d007e18645b15f507e270cb5415651b8a1
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-01-04 16:56:41 -0300

    SpriteSheet

--- js/Sprite.js
+++ js/Sprite.js
@@ -1,1 +1,13 @@
-//classe, atributos
+function Sprite(img){
+    //Atributos
+    this.srcX = this.srcY = 0;
+    this.width = 24;
+    this.height = 32;
+    this.posX = this.posY = 0;
+    this.img = img;
+
+    //Metodos
+    this.draw = function(ctx){
+        ctx.drawImage(this.img, this.srcX, this.srcY, this.width, this.height, this.posX, this.posY, this.width, this.height);
+    }
+}
--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 window.onload = function(){
+    var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40;
     var cnv = document.querySelector("canvas");
     var ctx =  cnv.getContext("2d");
+    var spriteSheet = new Image();
+    spriteSheet.src = "img/img.png";
 
-    alert(ctx);
+    var sapup3 = new Sprite(spriteSheet);
+
+    
 }
